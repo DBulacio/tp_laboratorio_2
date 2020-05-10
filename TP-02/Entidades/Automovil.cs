aae178b28abd66bb7d3c3de07ebe0ad73e122acf
DBulacio <daniel.bulacio@hotmail.com>
2020-05-10 17:06:41 -0300

    tp 2 completo

---
+++ TP-02/Entidades/Automovil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        public enum ETipo { Monovolumen, Sedan }
+        private ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            tipo = ETipo.Monovolumen;
+        }
+
+        public Automovil(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) 
+            : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("---------------------");
+            sb.AppendLine("");
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendFormat("TIPO   : {0}", this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+            sb.AppendLine("");
+
+            return sb.ToString();
+        }
+    }
+}
